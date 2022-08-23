a60eabee828be167fadc4e0058799bd64f114fd0
viher3 <viher_3@hotmail.com>
2022-08-23 23:01:11 +0200

    Installs bootstrap and blueprint vendors

--- src/Views/App.tsx
+++ src/Views/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
+import {Menu, MenuItem} from '@blueprintjs/core';
+import { BrowserRouter as Router, Route, Link, Routes, useRoutes } from "react-router-dom"
 import {SurveyListView} from "./Survey/List/SurveyListView";
+import '../App.css'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const App: React.FC = () => {
 
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                    </ul>
-                </nav>
+    const AppRoutes = () => {
+        return useRoutes([
+            { path: "/", element: <SurveyListView /> },
+            // ...
+        ])
+    };
 
-                <Routes>
-                    <Route path="/" element={<SurveyListView />} />
-                </Routes>
-            </div>
-        </Router>
+    return (
+
+        <Container>
+            <Row>
+                <Col sm={4}>
+                    <Menu>
+                        <MenuItem icon="new-text-box" text="New text box" />
+                        <MenuItem icon="new-object" text="New object" />
+                        <MenuItem icon="new-link" text="New link" />
+                    </Menu>
+                        {/*<Link to="/">Home</Link>*/}
+                </Col>
+                <Col>
+                    <Router>
+                        <AppRoutes />
+                    </Router>
+                </Col>
+
+            </Row>
+
+        </Container>
     );
 
 }
